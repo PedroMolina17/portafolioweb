6267cc6f291bc77d62fe0e8372e034fc0544c578
Pedro Molina <pedro.molinanoa@gmail.com>
2023-12-05 23:46:25 -0500

    addddd

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.re_bGdebf34_EasgQQR3TtgDYEP821wyxBPX);
+const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: fromEmail,
@@ -22,7 +23,6 @@ export async function POST(req, res) {
     });
     return NextResponse.json(data);
   } catch (error) {
-    console.log(error);
     return NextResponse.json({ error });
   }
 }
