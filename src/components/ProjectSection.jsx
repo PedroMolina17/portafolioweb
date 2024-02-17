fbb16139bdbc8667ec834341f9852e8d654c9f19
pedromolina17 <pedro.molinanoa@gmail.com>
2024-02-17 07:46:00 -0500

    Project 8

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -7,6 +7,15 @@ const projectData = [
   {
     id: "1",
     title: "Ecommerce",
+    description: "Typescript - Tailwind",
+    image: "/images/projects/Project_8.png",
+    tag: ["Projects"],
+    gitUrl: "https://github.com/PedroMolina17/ecommerce_typescript",
+    previewUrl: "/images/projects/Project_8.png",
+  },
+  {
+    id: "2",
+    title: "Ecommerce",
     description: "DjangoRest - ReactRedux - Tailwind",
     image: "/images/projects/Project_5.png",
     tag: ["Projects"],
@@ -14,7 +23,7 @@ const projectData = [
     previewUrl: "/images/projects/Project_5.png",
   },
   {
-    id: "2",
+    id: "3",
     title: "Copy of Tinder",
     description: "React - Tailwind",
     image: "/images/projects/Project_6.png",
@@ -23,7 +32,7 @@ const projectData = [
     previewUrl: "/images/projects/Project_6.png",
   },
   {
-    id: "3",
+    id: "4",
     title: "Tic Tac",
     description: "React- Tailwind",
     image: "/images/projects/Project_7.png",
@@ -32,7 +41,7 @@ const projectData = [
     previewUrl: "/images/projects/Project_7.png",
   },
   {
-    id: "4",
+    id: "5",
     title: "Task Crud",
     description: "Django - Python - Boostrap",
     image: "/images/projects/Project_3.png",
@@ -41,7 +50,7 @@ const projectData = [
     previewUrl: "https://django-auth-crud-y8x5.onrender.com/",
   },
   {
-    id: "5",
+    id: "6",
     title: "Portfolio Web",
     description: "React + Tailwind",
     image: "/images/projects/Project_2.png",
@@ -50,7 +59,7 @@ const projectData = [
     previewUrl: "/images/projects/Project_2.png",
   },
   {
-    id: "6",
+    id: "7",
     title: "Crud React",
     description: "React + SQL + Bootstrap",
     image: "/images/projects/Project_1.png",
@@ -59,7 +68,7 @@ const projectData = [
     previewUrl: "/images/projects/Project_1.png",
   },
   {
-    id: "7",
+    id: "8",
     title: "Catalogue",
     description: "React + Tailwind",
     image: "/images/projects/Project_4.png",
@@ -69,7 +78,7 @@ const projectData = [
   },
 
   {
-    id: "8",
+    id: "9",
     title: "Cognitive Class",
     description: "SQL and Relational Database",
     image: "/images/certificates/Certificate_1c.png",
@@ -78,7 +87,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_1c.png",
   },
   {
-    id: "9",
+    id: "10",
     title: "Microsoft",
     description: "Fundamentos de desarrollo",
     image: "/images/certificates/Certificate_2c.png",
@@ -87,7 +96,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_2c.png",
   },
   {
-    id: "10",
+    id: "11",
     title: "AWS",
     description: "Getting started on Data Analitycs",
     image: "/images/certificates/Certificate_3c.png",
@@ -96,7 +105,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_3c.png",
   },
   {
-    id: "11",
+    id: "12",
     title: "Oracle",
     description: "Oracle Cloud Data Management",
     image: "/images/certificates/Certificate_12c.png",
@@ -105,7 +114,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_12c.png",
   },
   {
-    id: "12",
+    id: "13",
     title: "Fundación Telefonica",
     description: "HTML5 + CSS",
     image: "/images/certificates/Certificate_4c.png",
@@ -114,7 +123,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_4c.png",
   },
   {
-    id: "13",
+    id: "14",
     title: "Fundación Telefonica",
     description: "Programacion con JavaScript",
     image: "/images/certificates/Certificate_5c.png",
@@ -123,7 +132,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_5c.png",
   },
   {
-    id: "14",
+    id: "15",
     title: "Fundación Telefonica",
     description: "Wordpress",
     image: "/images/certificates/Certificate_6c.png",
@@ -132,7 +141,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_6c.png",
   },
   {
-    id: "15",
+    id: "16",
     title: "Mimo",
     description: "Web Development",
     image: "/images/certificates/Certificate_7c.png",
@@ -141,7 +150,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_7c.png",
   },
   {
-    id: "16",
+    id: "17",
     title: "Mimo",
     description: "PYTHON",
     image: "/images/certificates/Certificate_8c.png",
@@ -150,7 +159,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_8c.png",
   },
   {
-    id: "17",
+    id: "18",
     title: "Mimo",
     description: "SQL",
     image: "/images/certificates/Certificate_9c.png",
@@ -159,7 +168,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_9c.png",
   },
   {
-    id: "18",
+    id: "19",
     title: "Mimo",
     description: "HTML",
     image: "/images/certificates/Certificate_10c.png",
@@ -168,7 +177,7 @@ const projectData = [
     previewUrl: "/images/certificates/Certificate_10c.png",
   },
   {
-    id: "19",
+    id: "20",
     title: "Mimo",
     description: "JavaScript",
     image: "/images/certificates/Certificate_11c.png",
